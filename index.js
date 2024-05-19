9bbcca0def78d22992e67b23dbdc6669bf36350b
maheshgamersop <141534592+maheshgamersop@users.noreply.github.com>
2024-05-19 09:58:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use('',express.static(path.join(__dirname, 'public')));
 
 app.get('/', (request, response) => {
-	return response.sendFile('index.html', { root: '.' });
+	return response.sendFile('lol.html', { root: '.' });
 });
 
 app.get('/auth/discord', (request, response) => {
